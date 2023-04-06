405fe5bb04a05ddf605478121c10c70627576ed2
Musfirah Waseem <musfirahwaseem0@gmail.com>
2023-04-06 14:29:40 +0500

    FEAT: Assignment

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+// Assignment
+// if-else
+// let age = +prompt("What is your age");
+// if(age <=10){
+// document.write("Welcome!")
+// }else{
+//     document.write("You are not allowed")
+// }
+
+
+// Table
